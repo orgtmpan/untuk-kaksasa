6d0278dc076e19954c59102399892aa98ee8cc7e
orgtmpan <irvaku007@gmail.com>
2026-06-02 15:02:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -83,3 +83,31 @@ async function loadSurat(){
     }
 
 }
+function createHeart(){
+
+    const heart =
+    document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "💗";
+
+    heart.style.left =
+    Math.random() * 100 + "vw";
+
+    heart.style.animationDuration =
+    (3 + Math.random() * 5) + "s";
+
+    document
+    .querySelector(".hearts")
+    .appendChild(heart);
+
+    setTimeout(() => {
+
+        heart.remove();
+
+    }, 8000);
+
+}
+
+setInterval(createHeart, 500);

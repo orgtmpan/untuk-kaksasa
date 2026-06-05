c5e60a437e309bfd03dca30e83248d21ceebad6f
orgtmpan <irvaku007@gmail.com>
2026-06-06 01:31:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -131,3 +131,49 @@ document
 ```
 
 }
+
+function updateCountdown(){
+
+```
+const target =
+new Date("2026-06-14 00:00:00");
+
+const now =
+new Date();
+
+const diff =
+target - now;
+
+if(diff <= 0){
+
+    document
+    .getElementById("countdown")
+    .innerHTML =
+    "🎉 Selamat Ulang Tahun Kak Sasa! 🎂💗";
+
+    return;
+}
+
+const days =
+Math.floor(diff / (1000 * 60 * 60 * 24));
+
+const hours =
+Math.floor((diff / (1000 * 60 * 60)) % 24);
+
+const minutes =
+Math.floor((diff / (1000 * 60)) % 60);
+
+const seconds =
+Math.floor((diff / 1000) % 60);
+
+document
+.getElementById("countdown")
+.innerHTML =
+`🎂 ${days} Hari ${hours} Jam ${minutes} Menit ${seconds} Detik`;
+```
+
+}
+
+setInterval(updateCountdown, 1000);
+
+updateCountdown();
